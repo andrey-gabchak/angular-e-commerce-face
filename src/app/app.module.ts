a331bc9792d9896f687ba911d62a11df88de96d9
gabchak <andrey.gabchak@gmail.com>
2018-11-22 21:38:45 +0200

    Lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,26 @@ import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import {CategoryService} from './services/category.service';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import {FormsModule} from '@angular/forms';
+
+const appRouts: Routes = [
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'registration', component: RegistrationComponent}
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    RegistrationComponent
   ],
   imports: [
+    RouterModule.forRoot(appRouts),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [CategoryService],
   bootstrap: [AppComponent]
