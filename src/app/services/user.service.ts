20f24b8a126869829e94636597e4091780cdefe8
gabchak <andrey.gabchak@gmail.com>
2018-11-26 19:56:46 +0200

    Create user.service with method getByEmail

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../model/user';
+
+@Injectable
+export class UserService {
+
+  constructor(private http: HttpClient) {}
+
+  public getByEmail(email: string): Observable<User> {
+    return this.http.get('http://localhost:8080/user', { params: new HttpParams().set('email', email)});
+  }
+}
